c1747e8321139a624c983a17cc62dfc77ffea6fd
Saturday <oncwnuBiDQl-vNhcN9TDz7_EWfNo@git.weixin.qq.com>
2025-08-08 01:54:24 +0800

    临时修改ip启动

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,7 +24,7 @@ export default function App() {
   }>({
     apiUrl: import.meta.env.DEV
       ? "http://localhost:2024"
-      : "http://localhost:8123",
+      : "http://43.160.193.72:8123",
     assistantId: "agent",
     messagesKey: "messages",
     onUpdateEvent: (event: any) => {
